2bfccf39b99ffc7492d0ae711a5ac3928c2df2b3
alejandrocardenasa-lgtm <alejandro.cardenas_a@uao.edu.co>
2025-11-13 15:51:47 -0500

    Update pqrsController.js

--- backend_scrapperfumestore/microservicios_pqrs/src.pqrs/controllers/pqrsController.js
+++ backend_scrapperfumestore/microservicios_pqrs/src.pqrs/controllers/pqrsController.js
@@ -3,11 +3,12 @@ const router = Router();
 const pqrsModel = require('../models/pqrsModel');
 const axios = require('axios');
 
+// Crea una nueva PQRS después de validar que el usuario exista
 router.post('/api/pqrs', async (req, res) => {
   try {
     const { usuarioId, tipo, descripcion } = req.body;
     
-    const usuarioResponse = await axios.get(`http://localhost:3001/api/usuarios/${usuarioId}`);
+    const usuarioResponse = await axios.get(`http://proyecto_final_microservicios_usuarios:3001/api/usuarios/${usuarioId}`);
     if (usuarioResponse.status !== 200 || !usuarioResponse.data) {
         return res.status(404).json({ mensaje: "Usuario no encontrado" });
     }
@@ -20,6 +21,7 @@ router.post('/api/pqrs', async (req, res) => {
   }
 });
 
+// Actualiza el estado o respuesta de una PQRS existente
 router.put('/api/pqrs/:id', async (req, res) => {
     try {
         const { id } = req.params;
@@ -32,6 +34,7 @@ router.put('/api/pqrs/:id', async (req, res) => {
     }
 });
 
+// Obtiene todas las PQRS asociadas a un usuario específico
 router.get('/api/pqrs/usuario/:usuarioId', async (req, res) => {
     try {
         const pqrs = await pqrsModel.obtenerPqrsPorUsuario(req.params.usuarioId);
@@ -42,15 +45,17 @@ router.get('/api/pqrs/usuario/:usuarioId', async (req, res) => {
     }
 });
 
+// Obtiene todas las PQRS existentes en el sistema
 router.get('/api/pqrs/all', async (req, res) => {
     try {
         const pqrs = await pqrsModel.obtenerTodasPqrs();
         res.status(200).json(pqrs);
     } catch (error) {
-        res.status(500).send(err);
+        res.status(500).send(error);
     }
 });
 
+// Elimina una PQRS por su ID
 router.delete('/api/pqrs/:id', async (req, res) => {
     try {
         await pqrsModel.eliminarPqrs(req.params.id);
