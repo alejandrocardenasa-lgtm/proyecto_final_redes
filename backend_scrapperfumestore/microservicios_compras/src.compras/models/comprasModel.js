5bd33dbe45ca18e6e4d9f8d1e4f33880351fea7b
alejandrocardenasa-lgtm <alejandro.cardenas_a@uao.edu.co>
2025-11-13 16:14:59 -0500

    Update comprasModel.js

--- backend_scrapperfumestore/microservicios_compras/src.compras/models/comprasModel.js
+++ backend_scrapperfumestore/microservicios_compras/src.compras/models/comprasModel.js
@@ -1,15 +1,15 @@
 const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
-  host: 'localhost',
+  host: 'mariadb_compras',   // nombre del servicio en Docker Swarm
   user: 'root',
   password: '',
-  port: '3307',
+  port: 3306,                // puerto interno del contenedor
   database: 'compras_db'
 });
 
 async function crearCompra(usuarioId, productoId, cantidad, precioTotal) {
-  const result = await connection.query(
+  const [result] = await connection.query(
     'INSERT INTO compras (usuario_id, producto_id, cantidad, precio_total) VALUES (?, ?, ?, ?)',
     [usuarioId, productoId, cantidad, precioTotal]
   );
